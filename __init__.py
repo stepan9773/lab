1d337cf03c80a340753f2ae316bb7e58fb86f7af
stepan <stepan.d9773@gmail.com>
2020-10-29 19:48:35 +0200

    __init__ modify

--- __init__.py
+++ __init__.py
@@ -1,15 +1,9 @@
-__version__ = '0.1.0'
 from flask import Flask
 app = Flask(__name__)
-@app.route('/iter/<int:index>')
-def iter(index):
-    return  ' '.join([str(i) for i in range(index)])
+
 @app.route('/api/v1/<string:index>')
 def hello_index(index):
-
     return  'Hello World ' + index[str(index).rfind("-")+1:] , print(" HTTP/1.1 OK 200")
 
 if __name__ == '__main__':
-    import sys
-    print(sys.executable)
     app.run(port=8000,host='localhost')
