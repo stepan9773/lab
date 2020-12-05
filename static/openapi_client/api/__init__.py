136c814c80c9432311fa0fe481197a0faee4e797
stepan9773 <stepan9773@users.noreply.github.com>
2020-12-05 02:28:27 +0200

    remaster lab 1

--- static/openapi_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.disciplines_api import DisciplinesApi
-from openapi_client.api.marks_api import MarksApi
-from openapi_client.api.rating_api import RatingApi
-from openapi_client.api.student_api import StudentApi
